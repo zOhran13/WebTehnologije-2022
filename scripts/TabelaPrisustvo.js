318d19b80c84434c1be69826cb7f38882ba2db70
zlata <zohran1@etf.unsa.ba>
2022-11-21 19:27:36 +0100

    Izrada tabele u 1.zadatku, greska <nisu izracunati procenti prisustva>

--- scripts/TabelaPrisustvo.js
+++ scripts/TabelaPrisustvo.js
@@ -1,20 +1,155 @@
- 
- 
- let TabelaPrisustvo = function (divRef, podaci) {
+let TabelaPrisustvo = function (divRef, podaci) {
+//$("divRef").empty();
+  //divRef.innerHTML="";
+    
+  divRef.innerHTML = ''
+        let tabela=document.createElement('table'); 
+        tabela.className = "mainTable"
+       
+        let prisustva = [];
+         Object.values(podaci).forEach(obj => {
+            if(obj instanceof Object) {
+                Object.values(obj).forEach(temp => {
+                    if(temp.hasOwnProperty("predavanja"))
+                    prisustva.push(temp)
+                })
+                
+            }
+        })
+        
+        let nijeUneseno = [];
+        Object.values(podaci).forEach(obj => {
+            if(obj instanceof Object) {
+                Object.values(obj).forEach(temp => {
+                    if(temp.hasOwnProperty("sedmica"))
+                    nijeUneseno.push(temp)
+                })
+                
+            }
+        })
+        let sedmiceUnesene = [...new Set(prisustva.map(prisustvo => prisustvo.sedmica))]; 
+        let sedmiceNisuUnesene = [...new Set(nijeUneseno.map(prisustvo => prisustvo.sedmica))]
+        let sedmice = sedmiceUnesene.concat(sedmiceNisuUnesene).reduce(
+            (unique, item) => (unique.includes(item) ? unique : [...unique, item]),
+            [],
+          );
 
+        let brojSedmica = sedmice.length;
+        let trenutnaSedmica = brojSedmica;
+
+        let zaglavlje = ['Ime i prezime', 'Index', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII'];
+        var header = document.createElement('thead')
+        header.appendChild(document.createElement("th")).
+        appendChild(document.createTextNode(zaglavlje[0]));
+
+        header.appendChild(document.createElement("th")).
+        appendChild(document.createTextNode(zaglavlje[1]));
+
+        for (let i=0; i<brojSedmica; i++) {
+            header.appendChild(document.createElement("th")).
+            appendChild(document.createTextNode(zaglavlje[i+2]));
+        }
+        if(brojSedmica < zaglavlje.length) {
+            header.appendChild(document.createElement("th")).
+            appendChild(document.createTextNode(zaglavlje[2 + brojSedmica] + "-" + zaglavlje[zaglavlje.length-1]));
+        }
+       
+        tabela.appendChild(header);
+
+        let studenti = []
+        Object.values(podaci).forEach(obj => {
+            if(obj instanceof Object) {
+                Object.values(obj).forEach(temp => {
+                    if(temp.hasOwnProperty("ime"))
+                    studenti.push(temp)
+                })
+                
+            }
+        })
+        let predmet = ""
+        Object.values(podaci).forEach(obj => {
+            if(!(obj instanceof Object) && typeof obj === "string") {
+                predmet = obj
+            }
+        })
+        let predavanjaSedmicno = podaci.brojPredavanjaSedmicno;
+        let vjezbiSedmicno = podaci.brojVjezbiSedmicno;
+
+        studenti.forEach(student => {
+            let red = document.createElement("tr");
+            let celija = document.createElement("td");
+            let ime = document.createTextNode(student.ime);
+            celija.appendChild(ime);
+            red.appendChild(celija);
+
+            celija = document.createElement("td");
+            let index = document.createTextNode(student.index)
+            celija.appendChild(index);
+            red.appendChild(celija);
+
+            let studentovaPrisustva = prisustva.filter(prisustvo => prisustvo.index === student.index && prisustvo.sedmica === trenutnaSedmica)
+            console.log(studentovaPrisustva)
+
+            
+            for(let k = 1; k <= brojSedmica; k++) {
+            if(k  === trenutnaSedmica) {
+            celija = document.createElement("td");
+
+            celija.className = "prisustvo"
+            let prviRed = document.createElement('tr')
+            let drugiRed = document.createElement('tr')
+            for(let i = 0; i < predavanjaSedmicno; i++) {
+                let celijaPredavanja = document.createElement("td")
+                celijaPredavanja.className="predavanja"
+                let predavanja = document.createTextNode("P"  +(i+1))
+                celijaPredavanja.appendChild(predavanja)
+                prviRed.appendChild(celijaPredavanja)
+            
+                let prisustvoPredavanja = document.createElement("td")
+                studentovaPrisustva.forEach(prisustvo => {
+                        let bioNaP = prisustvo.predavanja;
+                        if(i >= 0 && i < bioNaP)
+                        prisustvoPredavanja.className = "prisutan"
+                        else 
+                        prisustvoPredavanja.className = "odsutan"
+                })
+                drugiRed.appendChild(prisustvoPredavanja)
+
+            }
+            for(let i = 0; i < vjezbiSedmicno; i++) {
+                let celijaVjezbe = document.createElement("td")
+                celijaVjezbe.className="vjezbe"
+                let vjezbe = document.createTextNode("V" + (i+1))
+                celijaVjezbe.append(vjezbe)
+                prviRed.appendChild(celijaVjezbe)
+
+                let prisustvoVjezbe = document.createElement("td")
+                studentovaPrisustva.forEach(prisustvo => {
+                    let bioNaV = prisustvo.vjezbe
+                    if(i >= 0 && i < bioNaV)
+                    prisustvoVjezbe.className = "prisutan"
+                    else 
+                    prisustvoVjezbe.className = "odsutan"
+            })
+                drugiRed.appendChild(prisustvoVjezbe)
+        }
+        celija.appendChild(prviRed) 
+        celija.appendChild(drugiRed) 
+    }
+        else {
+            //postotak
+           celija = document.createElement("td");
+        }
+        red.appendChild(celija)
+        tabela.appendChild(red);
+        }
+        })
+      
+        divRef.appendChild(tabela);
+        
   
-divRef.innerHTML="";
-    
-document.write('<table>');
-document.write("<th>" + 'Ime i prezime' + "</th>");
-document.write("<th>" + 'Index' + "</th>");
 
-for( i=1; i<=Object.values(podaci).length(); i++) {
-    document.write( "<td" + i + "</td>" ); //pokusavam bilo sta sa objektom podaci
-    
-    
-}
-document.write('</table>');
+
 
 /*document.write('</table>');
         
@@ -47,7 +182,13 @@ document.write('</table>');
     
 
     }
+     
+    
     };
+    export {
+        TabelaPrisustvo
+    };
+   
     
   
  
--- scripts/prisustvo.js
+++ scripts/prisustvo.js
@@ -1,11 +1,49 @@
+
 let div = document.getElementById("divSadrzaj");
 //instanciranje
 
     
-let prisustvo = TabelaPrisustvo(div, {studenti: [{ime:"Neko",index:12345}],
-prisustva:[{sedmica:1,predavanja:1,vjezbe:1,index:12345}], predmet:"WT",
-brojPredavanjaSedmicno:3, brojVjezbiSedmicno:2});
+let prisustvo = TabelaPrisustvo(div, {
+    "studenti": [{
+    "ime": "Neko Nekic",
+    "index": 12345
+    },
+    {
+    "ime": "Drugi Neko",
+    "index": 12346
+    }
+    ],
+    "prisustva": [{
+    "sedmica": 1,
+    "predavanja": 2,
+    "vjezbe": 1,
+    "index": 12345
+    },
+    {
+    "sedmica": 1,
+    "predavanja": 2,
+    "vjezbe": 2,
+    "index": 12346
+    },
+    {
+    "sedmica": 2,
+    "predavanja": 2,
+    "vjezbe": 0,
+    "index": 12345
+    },
+    {
+    "sedmica": 2,
+    "predavanja": 2,
+    "vjezbe": 0,
+    "index": 12346
+    }
+    ],
+    "predmet": "Razvoj mobilnih aplikacija",
+    "brojPredavanjaSedmicno": 2,
+    "brojVjezbiSedmicno": 2
+    }
+    );
 //pozivanje metoda
-
+import {TabelaPrisustvo} from '../scripts/TabelaPrisustvo.js'
 prisustvo.sljedecaSedmica();
 prisustvo.prethodnaSedmica();
